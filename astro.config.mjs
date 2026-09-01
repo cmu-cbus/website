8b9f651301df8c2618291b7e4fc026c881d1b870
Krit Dass <dasskrit@gmail.com>
2026-08-31 21:50:25 -0400

    feat: add github pages setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://cmu-cbus.github.io",
+  base: "/website",
   prefetch: {
     prefetchAll: true,
     defaultStrategy: 'hover',
