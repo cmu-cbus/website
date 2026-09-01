bd70dde6a20adc22cebaa84c5abe2f8ed220f2da
Krit Dass <dasskrit@gmail.com>
2026-08-31 22:34:10 -0400

    fix: update links

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://cmu-cbus.github.io",
-  base: "/website",
+  site: "https://cmu-cbus.org",
+  base: "/",
   prefetch: {
     prefetchAll: true,
     defaultStrategy: 'hover',
