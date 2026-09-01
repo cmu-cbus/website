b1c301cc034175f0c2696f73b145cbfe88021ca4
Krit Dass <dasskrit@gmail.com>
2026-08-31 22:54:27 -0400

    chore: add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,21 @@ import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://cmu-cbus.org",
   base: "/",
+
   prefetch: {
     prefetchAll: true,
     defaultStrategy: 'hover',
   },
+
   vite: {
     plugins: [tailwindcss()],
   },
+
+  integrations: [sitemap()],
 });
